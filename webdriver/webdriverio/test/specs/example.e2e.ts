5dfe700497932f6167f24bcdb10bd1b6f1d63606
Alex Rock Ancelet <pierstoval@gmail.com>
2022-03-23 21:11:12 +0100

    Run prettier on the project

--- webdriver/webdriverio/test/specs/example.e2e.ts
+++ webdriver/webdriverio/test/specs/example.e2e.ts
@@ -1,13 +1,13 @@
-describe("Hello Tauri", () => {
-  it("should be cordial", async () => {
-    const header = await $("body h2");
-    const text = await header.getText();
-    expect(text).toMatch(/^[hH]ello/);
-  });
+describe('Hello Tauri', () => {
+    it('should be cordial', async () => {
+        const header = await $('body h2');
+        const text = await header.getText();
+        expect(text).toMatch(/^[hH]ello/);
+    });
 
-  it("should be excited", async function () {
-    const header = await $("body h2");
-    const text = await header.getText();
-    expect(text).toMatch(/!$/);
-  });
+    it('should be excited', async function () {
+        const header = await $('body h2');
+        const text = await header.getText();
+        expect(text).toMatch(/!$/);
+    });
 });
